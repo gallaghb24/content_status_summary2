3c07a455b8da950fb51942d4765b6b6038fab527
gallaghb24 <ben@gallag.co.uk>
2025-04-10 20:45:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 import streamlit as st
+st.set_page_config(page_title="Event Artwork Status Report", layout="wide")
+
+st.markdown(
+    """
+    <style>
+    h1, h2, h3, h4, h5, h6 {
+        visibility: visible;
+    }
+    .stMarkdown .css-1aumxhk a {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 st.markdown(
     """
@@ -17,7 +32,6 @@ import pandas as pd
 from io import BytesIO
 from datetime import datetime
 
-st.set_page_config(page_title="Event Artwork Status Report", layout="wide")
 st.title("📊 Event Artwork Status Report")
 
 st.markdown("""
