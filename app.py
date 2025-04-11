8702f394a7cb594a45c261c5a005a4cd1f57bc06
gallaghb24 <ben@gallag.co.uk>
2025-04-11 10:41:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -41,8 +41,8 @@ st.markdown("""
 1. Go to the [**Production Lines report**](https://superdrug.aswmediacentre.com/Reports/Reports/CustomReport?reportId=2) in Media Centre.  
 2. Type the **exact name of the Event** you want to report on (e.g. *Event 6 2025*), or choose a **date range** if you'd like to include multiple Events.  
 3. Click **Search** to generate the results.  
-4. Once the data loads, click the **Excel icon** to export the file. Then leave it as HTML and click **Export** on the pop-up window.  
-5. When the file has downloaded, **open it** and then **Save As** an `.xlsx` Excel file (not `.xls` or `.csv`).  
+4. Once the data loads, click the **Excel icon** to export the file. On the pop-up, leave it as HTML and click **Export**.  
+5. Open the downloaded `.xls` file, re-save it as `.xlsx` Excel file (not `.xls` or `.csv`).  
 6. You're now ready — upload the `.xlsx` file using the uploader below.
 """)
 
